844647110b4488a269a59259c0551d18ee8b583d
pridhorajiyagithub <111557931+pridhorajiyagithub@users.noreply.github.com>
2022-11-25 12:15:59 +0530

    Update quot.dart

--- lib/model/quot.dart
+++ lib/model/quot.dart
@@ -4,6 +4,7 @@ class Quot {
   final String quot;
   final String author;
   final Uint8List image;
+  //final String image;
 
   Quot({required this.image, required this.quot, required this.author});
 
@@ -20,14 +21,22 @@ class DBQuot {
   final String quot;
   final String author;
   late Uint8List image;
+  //late String image;
+  String? backgroungimage;
 
-  DBQuot({required this.image, required this.quot, required this.author});
+  DBQuot(
+      {required this.image,
+      required this.quot,
+      required this.author,
+      this.backgroungimage});
 
   factory DBQuot.fromJSON(Map data) {
     return DBQuot(
       image: data["image"],
       quot: data["quote"],
       author: data["author"],
+      backgroungimage:
+          "https://love-shayari.co/wp-content/uploads/2021/10/sun-rise.jpg",
     );
   }
 }
